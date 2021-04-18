af0a8ab532d04aee6ef090c4a6c98cbfa4353a76
RutPython <nirut1981@gmail.com>
2021-04-18 17:37:12 +0700

    Create Exercise4_Nirut_K.py
    Create Exercise4

---
+++ Exercise4_Nirut_K.py
@@ -0,0 +1,14 @@
+Subject1 = "Foundation English"
+Subject2 = "General Business"
+Subject3 = "Introduction to Computer Systems"
+Subject4 = "Computer Programming"
+Score1 = 81.0
+Score2 = 82.0
+Score3 = 83.0
+Score4 = 84.0
+
+print("--- Your Score ---")
+print(Subject1," : ", Score1)
+print(Subject2," : ", Score2)
+print(Subject3," : ", Score3)
+print(Subject4," : ", Score4)
